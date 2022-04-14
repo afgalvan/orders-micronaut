f63b814b7837f5a838ebfca537f3c232f492c77b
Andres Galvan <andresgalfajar@gmail.com>
2022-04-14 13:45:12 -0500

    ci(tests): just run unit tests for now

--- Productos/src/test/java/tech/afgalvan/productos/integration/ControllerSteps.java
+++ Productos/src/test/java/tech/afgalvan/productos/integration/ControllerSteps.java
@@ -5,16 +5,15 @@ import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.micronaut.context.ApplicationContext;
 import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.HttpClient;
-import io.micronaut.runtime.EmbeddedApplication;
-import io.micronaut.runtime.server.EmbeddedServer;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,16 +21,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ControllerSteps {
 
     private static HttpClient client;
-    private static ApplicationContext context;
     HttpRequest<?> request;
     HttpResponse<?> response;
     private final ObjectMapper mapper = new ObjectMapper();
 
     @BeforeAll
-    public static void setup() {
-        context = ApplicationContext.run();
-        EmbeddedServer server = context.getBean(EmbeddedServer.class).start();
-        client = context.createBean(HttpClient.class, server.getURL());
+    public static void setup() throws MalformedURLException {
+        client = HttpClient.create(new URL("http://localhost:9000"));
     }
 
     @When("I send a POST request to {string} with body:")
@@ -58,6 +54,5 @@ public class ControllerSteps {
     @AfterAll
     public static void tearDown() {
         client.stop();
-        context.stop();
     }
 }
